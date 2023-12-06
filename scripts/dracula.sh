8d94cf0967c121218983d6ef722d8cf2812f1662
Akiba, S. / ゐてるま <anosatsuk124@gmail.com>
2023-12-07 06:56:48 +0900

    Make a window name display CWD
    It's a customization for me.

--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -285,12 +285,20 @@ main()
 
   # Window option
   if $show_powerline; then
-    tmux set-window-option -g window-status-current-format "#[fg=${gray},bg=${dark_purple}]${left_sep}#[fg=${white},bg=${dark_purple}] #I #W${current_flags} #[fg=${dark_purple},bg=${gray}]${left_sep}"
+  
+    ## Show CWD
+    script="#($current_dir/cwd.sh)"
+    tmux set-option -g automatic-rename on
+    tmux set-window-option -g window-status-format "#[fg=${white}]#[bg=${gray}] #I #{?#{m:#W,zsh},$script,#W}${flags}"
+    tmux set-window-option -g window-status-current-format "#[fg=${gray},bg=${dark_purple}]${left_sep}#[fg=${white},bg=${dark_purple}] #I #{?#{m:#W,zsh},$script,#W}${current_flags} #[fg=${dark_purple},bg=${gray}]${left_sep}"
+    ##
+    
+    # tmux set-window-option -g window-status-current-format "#[fg=${gray},bg=${dark_purple}]${left_sep}#[fg=${white},bg=${dark_purple}] #I #W${current_flags} #[fg=${dark_purple},bg=${gray}]${left_sep}"
   else
     tmux set-window-option -g window-status-current-format "#[fg=${white},bg=${dark_purple}] #I #W${current_flags} "
   fi
 
-  tmux set-window-option -g window-status-format "#[fg=${white}]#[bg=${gray}] #I #W${flags}"
+  # tmux set-window-option -g window-status-format "#[fg=${white}]#[bg=${gray}] #I #W${flags}"
   tmux set-window-option -g window-status-activity-style "bold"
   tmux set-window-option -g window-status-bell-style "bold"
 }
